a75fc7f14224b48f486a091d43890af450fe84a5
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-09-10 22:28:52 +0530

    a data preprocessing template

---
+++ Regression/1 - Simple Linear Regression/data_preprocessing_template.py
@@ -0,0 +1,28 @@
+"""
+This is just a data preprocessing template that you can use for your specific
+data for preprocessing.
+"""
+
+# Data Preprocessing
+
+#Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2, random_state = 0)
+
+# Feature Scaling (sometimes we need it, otherwise the model always take care of it.)
+"""
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+"""
