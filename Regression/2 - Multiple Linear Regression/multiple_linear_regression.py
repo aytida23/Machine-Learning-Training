ca3894232389ea2a9aa0d3316221c00c9ea76d37
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-09-10 22:30:41 +0530

    prepared the data for the multiple linear regression model

---
+++ Regression/2 - Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,34 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+# Encoding the independent variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 3] = labelencoder_x.fit_transform(x[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+x = onehotencoder.fit_transform(x).toarray()
+
+# Avoiding the Dummy Variable Trap
+x = x[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
