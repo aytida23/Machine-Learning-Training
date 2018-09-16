d0d87a43d33103ef1392ef575de883b3ff56a5e4
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-09-16 21:58:16 +0530

    Implementation of Random Forest Regression for non-linear regression model
    Random Forest is a version of Ensemble Learning, it really can predict a value at the right spot, here in this implementation this model predicted the exact previous salary of an employee while fitting the model with 300 trees for this dataset. Hence it shows the more trees you use the more accurate prediction you may get for your following dataset.

---
+++ Regression/6 - Random Forest Regression (RFR)/random_forest_regression.py
@@ -0,0 +1,47 @@
+"""
+This is the code implementation for random forest regression for non-linear regression model.
+Random Forest is a version of Ensemble learning.
+"""
+# Random Forest Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting the Random Forest Regression Model to the dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X, y)
+
+# Predicting a new result, we actually get the exact previous salary predicted and hitting at right spot
+# after using 300 trees for making the random forest regression model
+y_pred = regressor.predict(6.5)
+
+
+# Visualising the Random Forest Regression Model results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
