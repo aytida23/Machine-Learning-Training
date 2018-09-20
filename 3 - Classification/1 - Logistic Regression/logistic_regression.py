5cfe85f3c1237e9c64572cba2e4c96040520c0d2
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-09-20 17:09:10 +0530

    Preprocessed the data and fitted the logistic regression to the training set

---
+++ 3 - Classification/1 - Logistic Regression/logistic_regression.py
@@ -0,0 +1,30 @@
+"""
+This is the code implementation of Classification's part Logistic Regression Model.
+"""
+
+# Logistic Regression
+
+#Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.25, random_state = 0)
+
+# Feature Scaling (sometimes we need it, otherwise the model always take care of it.)
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(x_train, y_train)
