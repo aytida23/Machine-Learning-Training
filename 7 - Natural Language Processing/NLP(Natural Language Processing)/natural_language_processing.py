21e91f9ba877f42e4f2623f0c31845ceccb3aef1
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-11-19 21:29:02 +0530

    importing libraries and the dataset

---
+++ 7 - Natural Language Processing/NLP(Natural Language Processing)/natural_language_processing.py
@@ -0,0 +1,12 @@
+"""
+This is the implementation of NLP (Natural Language Processing) algorithm.
+"""
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
