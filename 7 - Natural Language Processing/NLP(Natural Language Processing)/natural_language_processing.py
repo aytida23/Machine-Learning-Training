f5dcfa93cd21b1b481d17e084134aa716892d335
Aditya Mandal <aditya.cse.nitap@gmail.com>
2018-11-19 22:21:31 +0530

    cleaning the texts like removing the useless english words with the help of stopwords function from nltk library

--- 7 - Natural Language Processing/NLP(Natural Language Processing)/natural_language_processing.py
+++ 7 - Natural Language Processing/NLP(Natural Language Processing)/natural_language_processing.py
@@ -1,5 +1,5 @@
 """
-This is the implementation of NLP (Natural Language Processing) algorithm.
+This is the implementation of NLP (Natural Language Processing) algorithm for text preprocessing.
 """
 # Natural Language Processing
 
@@ -10,3 +10,13 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+# Cleaning the Texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
+review = review.lower()
+review = review.split()
+review = [word for word in review if word not in set(stopwords.words('english'))]
